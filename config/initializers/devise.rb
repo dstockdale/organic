e621bb5db13ef025036479638a8ea84e4a823782
Dom Stockdale <doms.stockdale@gmail.com>
2015-08-11 12:26:31 +0100

    Adds secret key for devise

---
+++ config/initializers/devise.rb
@@ -0,0 +1,4 @@
+Devise.setup do |config|
+  config.secret_key = 'ce52c3bebee0c5f2e0ba4a556be8b836c158cd516f54bd106ba5a492314a00e472ffe5c377524bca53cd510b5e88e077543f8c3ce48b9325b028ff76a7e48bb3'
+  config.mailer_sender = 'hello@organiczoo.co.uk'
+end
